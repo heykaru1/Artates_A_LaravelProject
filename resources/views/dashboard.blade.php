26dddd1928adfdce983ff47f023a0dd3f4e93588
heykaru <akototoamil2@gmail.com>
2024-10-07 18:30:06 +0800

    edit dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <h3 class="text-red-700 font-bolder"
+                    >Hi Ammiel</h3>
+                    <p class="bg-blue-400 py-4 px-8 rounded-lg text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio laborum error a. Ullam numquam consectetur corporis assumenda accusamus, eaque unde?
+
+                    </p>
                 </div>
             </div>
         </div>
